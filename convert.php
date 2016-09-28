92332c92d6fc5a461275ecb8a5b5540c38c68b4e
Ashish Saxena <ashish@reak.in>
2016-09-28 17:24:14 +0530

    Initial Upload
    Synced Project to Github

--- convert.php
+++ convert.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * (C) Ashish Saxena <ashish@reak.in>
+ * (C) 2016 REAK INFOTECH LLP
+ *
+ * The LICENSE file included with the project would govern the use policy for this code,
+ * In case of missing LICENSE file the code will be treated as an Intellectual Property of the creator mentioned above,
+ * All rights related to distribution, modifcation, reselling, use for commercial or private use of this code is terminated.
+ *
+ */
+
+
+
 function makexml(){
 
 $out='
--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+
+/*
+ * (C) Ashish Saxena <ashish@reak.in>
+ * (C) 2016 REAK INFOTECH LLP
+ *
+ * The LICENSE file included with the project would govern the use policy for this code,
+ * In case of missing LICENSE file the code will be treated as an Intellectual Property of the creator mentioned above,
+ * All rights related to distribution, modifcation, reselling, use for commercial or private use of this code is terminated.
+ *
+ */
+
 <html>
 	<head><title>Internal Tool - Accounting Dept.</title></head>
 <body>
